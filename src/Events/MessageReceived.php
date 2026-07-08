9ca5abebe85827e0cbff2f42f161c5537395b6ad
Arthur <arthur@luoyueliang.com>
2026-07-08 00:49:02 +0000

    DEV: SPEC-017-dev

--- src/Events/MessageReceived.php
+++ src/Events/MessageReceived.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MultiTenantSaas\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use MultiTenantSaas\DTOs\MessageDTO;
 use MultiTenantSaas\Models\Message;
 
 class MessageReceived
@@ -15,4 +18,9 @@ class MessageReceived
         public readonly Message $message,
         public readonly string $channel = 'web',
     ) {}
+
+    public function toMessageDTO(): MessageDTO
+    {
+        return MessageDTO::fromModel($this->message, $this->channel);
+    }
 }

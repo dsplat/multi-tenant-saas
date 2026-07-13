386ea71c5e44454c18b891346d8d4f43ea675287
luoyueliang <luoyueliang@gmail.com>
2026-07-14 01:27:07 +0800

    refactor(operator): move IdentifyOperator middleware into Operator module
    - Move IdentifyOperator to src/Modules/Operator/Http/Middleware/
    - Update namespace to MultiTenantSaas\Modules\Operator\Http\Middleware
    - Update bootstrap/app.php to use new namespace

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,12 +23,12 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(prepend: [
             \MultiTenantSaas\Middleware\IdentifyTenant::class,
-            \MultiTenantSaas\Middleware\IdentifyOperator::class,
+            \MultiTenantSaas\Modules\Operator\Http\Middleware\IdentifyOperator::class,
         ]);
 
         $middleware->api(prepend: [
             \MultiTenantSaas\Middleware\IdentifyTenant::class,
-            \MultiTenantSaas\Middleware\IdentifyOperator::class,
+            \MultiTenantSaas\Modules\Operator\Http\Middleware\IdentifyOperator::class,
             \MultiTenantSaas\Middleware\SetLocale::class,
         ]);
 
